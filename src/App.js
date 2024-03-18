41e6ddcce462c3fd0cee88800d8f39a9df22fca4
Jordan Aasman <jjord@ad.unc.edu>
2024-03-18 19:44:57 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}! It's a nice day</div>;
+  return <div>Hello {value}! It's a nice day. How are you?</div>;
 }
 
 export default App;
